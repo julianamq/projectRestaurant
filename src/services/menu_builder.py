eacdbbdcbf2b2b08681b4fa657c28a93e10d058d
Juliana <julianamq20@gmail.com>
2023-05-16 14:04:18 -0300

    tentativa 4

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,15 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> pd.DataFrame:
-        pass
+        return pd.DataFrame(
+            [
+                {
+                    "dish_name": dish.name,
+                    "ingredients": dish.get_ingredients(),
+                    "price": dish.price,
+                    "restrictions": dish.get_restrictions(),
+                }
+                for dish in self.menu_data.dishes
+                if restriction not in dish.get_restrictions()
+            ]
+        )
